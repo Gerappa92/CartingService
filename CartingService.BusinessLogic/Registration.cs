25019ec3ab4b31ea80c185a387608209ded37887
Krzysztof Juszcze <ep_kjuszcze@phreesia.com>
2023-03-15 16:48:03 +0100

    Created registration classes

---
+++ CartingService.BusinessLogic/Registration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CartingService.BusinessLogic;
+
+public static class Registration
+{
+    public static IServiceCollection AddBusinessLogic(this IServiceCollection services)
+    {
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
---
+++ CartingService.DataAccess/Registration.cs
@@ -0,0 +1,13 @@
+﻿using CartingService.DataAccess.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CartingService.DataAccess;
+
+public static class Registration
+{
+    public static IServiceCollection AddDataAccess(this IServiceCollection services)
+    {
+        services.AddSingleton<ICartRepository, CartRepository>();
+        return services;
+    }
+}
